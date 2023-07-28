3d3beba2e17cd12b3cde53add1e9321ea4078bcd
Artem Vopilov <mister.0025@yandex.ru>
2023-07-29 00:58:13 +0300

    activity slot booking

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,3 +1,6 @@
+from .activity.activity import Activity
+from .activity.activity_slot import ActivitySlot
+from .activity.activity_slot_booking import ActivitySlotBooking
 from .business import Business
 from .service.service import Service
 from .service.service_slot import ServiceSlot
--- src/schemas/activity/activity_slot_booking.py
+++ src/schemas/activity/activity_slot_booking.py
@@ -1,7 +1,7 @@
 from mongoengine import Document, BooleanField, ObjectIdField
 
 
-class ActivityBooking(Document):
+class ActivitySlotBooking(Document):
     slot_id = ObjectIdField(required=True)
     user_id = ObjectIdField(required=True)
     is_accepted = BooleanField(default=False)
