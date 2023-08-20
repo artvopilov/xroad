cb538189c7c04dc405ef55e85b13ca9c1368e608
Artem Vopilov <mister.0025@yandex.ru>
2023-08-20 20:24:36 +0300

    add routes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from mongoengine import connect
 
-from src.routes import user_router, business_router, activity_router
+from src.routes import user_router, business_router, activity_router, slot_router, booking_router
 
 connect('test', host='127.0.0.1', port=27017)
 
@@ -10,3 +10,5 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(business_router)
 app.include_router(activity_router)
+app.include_router(slot_router)
+app.include_router(booking_router)
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,3 +1,5 @@
 from .activity import router as activity_router
+from .booking import router as booking_router
 from .business import router as business_router
+from .slot import router as slot_router
 from .user import router as user_router
