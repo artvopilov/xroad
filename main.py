223d1f9b87ea3414f6924fca0fee5f1a684f5c57
Artem Vopilov <mister.0025@yandex.ru>
2024-03-09 20:31:26 +0300

    base admin

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from mongoengine import connect
+from starlette_admin.contrib.mongoengine import Admin, ModelView
 
 from src.routes import user_router, activity_router
+from src.schemas import Activity, User
 
 connect('test', host='db', port=27017)
 
@@ -9,3 +11,10 @@ app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(activity_router)
+
+admin = Admin()
+
+admin.add_view(ModelView(User))
+admin.add_view(ModelView(Activity))
+
+admin.mount_to(app)
